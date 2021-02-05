d8271b0907c0be063404cda5505588198e490e09
Heo Jong Tae <heo9910@naver.com>
2021-02-05 23:47:21 +0900

    .

--- dict/dict.go
+++ dict/dict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errorNotFound = errors.New("Not found")
+var errWordExists = errors.New("New word already exists")
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +15,16 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errorNotFound
+}
+
+// add new word
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errorNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -8,6 +8,23 @@ import (
 )
 
 func main() {
+	// -- loop --
+	var numbers = []int{}
+	for i := 1; i < 11; i++ {
+		numbers = append(numbers, i)
+	}
+	fmt.Println(numbers)
+
+	for index, number := range numbers {
+		fmt.Println(index, ":", number)
+	}
+
+	// index not used
+	for _, number := range numbers {
+		fmt.Println(number)
+	}
+
+	// -- loop --
 
 	// -- struct --
 	account := banking.NewAccount("nico")
@@ -39,6 +56,15 @@ func main() {
 		fmt.Println(definition)
 	} else {
 		fmt.Println(err)
+	}
+
+	err = dictionary.Add("hello", "Greeting")
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, err := dictionary.Search("hello")
+	if err != nil {
+		fmt.Println(hello)
 	}
 	// -- Dictionary --
 }
